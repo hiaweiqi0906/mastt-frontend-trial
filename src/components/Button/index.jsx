4d7d3e38e1171ad667b7596a0a2f72659f453462
hiaweiqi0906 <hiaweiqi@gmail.com>
2024-04-17 23:18:06 +0800

    Fix onclick btn component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import './Button.css'
 
-export default function MasttButton({type, label, className}) {
+export default function MasttButton({type, label, className, onClick}) {
   return (
-    <button className={'btn-primary ' + className}>{label}</button>
+    <button className={'btn-primary ' + className} onClick={onClick}>{label}</button>
   )
 }
