e5713074174abc4009b817ef5d4791e3f4e7d04c
hiaweiqi0906 <hiaweiqi@gmail.com>
2024-04-25 21:20:39 +0800

    Add Club Profile page and Ranking page

--- src/pages/Common/Profile/index.jsx
+++ src/pages/Common/Profile/index.jsx
@@ -6,20 +6,24 @@ import Stack from "react-bootstrap/Stack";
 import TextField from "components/Input/TextField";
 import Button from "components/Button";
 import Card from "components/Card";
-
 import ViewIcon from "assets/icons/eye.svg";
 import FireIcon from "assets/icons/fire.svg";
 import LikeIcon from "assets/icons/hand-thumbs-up.svg";
 import MaleIcon from "assets/icons/gender-male.svg";
 import testImg from "assets/Avatar.png";
 import "./Profile.css";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Profile({ userType }) {
+  const navigate = useNavigate()
+  const handleClickMatch = () => {
+    navigate('/match-details')
+  }
   return (
     <Container style={{ marginTop: "3rem" }}>
       {userType === "player" && (
         <>
-          <Card className="card-xxl card-height" style={{marginBottom: "24px"}}>
+          <Card className="card-xxl card-height" style={{ marginBottom: "24px" }}>
             <Stack direction="horizontal" gap={3}>
               <img src={testImg} alt="" width={100} height={100} />
               <div style={{ marginLeft: "2rem" }}>
@@ -95,12 +99,12 @@ export default function Profile({ userType }) {
               </Card>
             </Col>
             <Col xs={8}>
-              <Card style={{height: "100%"}}>
+              <Card style={{ height: "100%" }}>
                 <Stack direction="horizontal" gap={2}>
                   <p>Match History</p>
                   <p className="ms-auto">All</p>
                 </Stack>
-                <div style={{marginBottom: "24px"}}>
+                <div style={{ marginBottom: "24px" }}>
                   <Stack direction="horizontal" gap={4}>
                     <img src={testImg} alt="" width={56} height={56} />
                     <span className="font-20 font-bold">11</span>
@@ -108,12 +112,12 @@ export default function Profile({ userType }) {
                     <span className="font-20 font-bold">7</span>
                     <img src={testImg} alt="" width={56} height={56} />
                     <div className="ms-auto">
-                      <img src={ViewIcon} alt="" width={16} height={16} style={{display: "block", marginLeft: "auto"}}/>
+                      <img src={ViewIcon} alt="" width={16} height={16} style={{ display: "block", marginLeft: "auto" }} />
                       <p>Match Date Time</p>
                     </div>
                   </Stack>
                 </div>
-                <div style={{marginBottom: "24px"}}>
+                <div style={{ marginBottom: "24px" }}>
                   <Stack direction="horizontal" gap={4}>
                     <img src={testImg} alt="" width={56} height={56} />
                     <span className="font-20 font-bold">11</span>
@@ -121,12 +125,12 @@ export default function Profile({ userType }) {
                     <span className="font-20 font-bold">7</span>
                     <img src={testImg} alt="" width={56} height={56} />
                     <div className="ms-auto">
-                      <img src={ViewIcon} alt="" width={16} height={16} style={{display: "block", marginLeft: "auto"}}/>
+                      <img src={ViewIcon} alt="" width={16} height={16} style={{ display: "block", marginLeft: "auto" }} />
                       <p>Match Date Time</p>
                     </div>
                   </Stack>
                 </div>
-                <div style={{marginBottom: "24px"}}>
+                <div style={{ marginBottom: "24px" }}>
                   <Stack direction="horizontal" gap={4}>
                     <img src={testImg} alt="" width={56} height={56} />
                     <span className="font-20 font-bold">11</span>
@@ -134,7 +138,7 @@ export default function Profile({ userType }) {
                     <span className="font-20 font-bold">7</span>
                     <img src={testImg} alt="" width={56} height={56} />
                     <div className="ms-auto">
-                      <img src={ViewIcon} alt="" width={16} height={16} style={{display: "block", marginLeft: "auto"}}/>
+                      <img src={ViewIcon} alt="" width={16} height={16} style={{ display: "block", marginLeft: "auto" }} />
                       <p>Match Date Time</p>
                     </div>
                   </Stack>
@@ -146,29 +150,152 @@ export default function Profile({ userType }) {
       )}
       {userType === "club" && (
         <>
-          <Card>
+          <Card className="card-xxl card-height" style={{ marginBottom: "24px" }}>
+            <Stack direction="horizontal" gap={3}>
+              <img src={testImg} alt="" width={100} height={100} />
+              <div style={{ marginLeft: "2rem" }}>
+                <Stack direction="horizontal" gap={2}>
+                  <p
+                    className="font-24 font-bold"
+                    style={{ marginBottom: "5px" }}
+                  >
+                    {"Jesselball Club"}
+                  </p>
+                  <p className="font-16" style={{ marginBottom: "0" }}>
+                    {", Simpang Ampat"}
+                  </p>
+                </Stack>
+                <p className="font-16" style={{ color: "#AFAFAF" }}>
+                  {"joined from 3 March 2023"}
+                </p>
+              </div>
+              <div className="p-2 ms-auto" style={{ textAlign: "center" }}>
+                <img src={FireIcon} alt="" width={36} height={36} />
+                <p className="font-16">{"40k"}</p>
+              </div>
+              <div className="p-2" style={{ textAlign: "center" }}>
+                <img src={LikeIcon} alt="" width={36} height={36} />
+                <p className="font-16">{"400.1k"}</p>
+              </div>
+            </Stack>
+          </Card>
+          {/* <Card>
             <img src="" alt="" width={64} height={64} />
             <p>{"Club's Name"}</p>
             <p>{"Club's Location"}</p>
             <p>{"Club's Joined Date"}</p>
             <p>{"Club's Followers"}</p>
-          </Card>
-          <Card>
-            <p>Booking</p>
-            <p>Book Now</p>
-            <p>Competition</p>
-            <div>
-              <img src="" alt="" width={64} height={64} />
-              <p>Competition Name</p>
-              <p>Competition Datetime</p>
-              <p>Sign-Up Now</p>
-            </div>
-          </Card>
-          <Card>
-            <p>Posted by Club Name</p>
-            <p>Posted timeline</p>
-            <p>Posted Content</p>
-          </Card>
+          </Card> */}
+          <Row>
+            <Col xs={4}>
+              <Card style={{ height: "100%" }}>
+                <Stack direction='horizontal' gap={0}>
+                  <p>Booking</p>
+                  <div className='ms-auto' style={{ textAlign: 'right' }}>
+                    <Link style={{ color: "black" }}><p>Sign Up Now {">"}</p></Link>
+                  </div>
+                </Stack>
+                <Stack
+                  direction="horizontal"
+                  gap={2}
+                  style={{ justifyContent: "center", height: '200px' }}
+                >
+                  <p className="font-32 font-bold">Price List</p>
+                  <p className="font-12" style={{ marginBottom: "5px" }}>
+                  </p>
+                </Stack>
+                <p>Competition</p>
+                <div style={{ cursor: "pointer", borderBottom: '#d6d6d6 solid 0.5px', marginBottom: '1rem' }} onClick={handleClickMatch}>
+                  <Stack direction="horizontal">
+                    <div>
+                      <p className='font-bold truncate-1-lines'>Table Tennis Ranking Match - APRIL 2024</p>
+
+                      <Stack direction="horizontal">
+                        <p className='truncate-1-lines font-14'>1 May 2024 - 31 May 2024</p>
+                        <div className='ms-auto' style={{ textAlign: 'right' }}>
+                          <Link style={{ color: "black" }}><p>Sign Up Now {">"}</p></Link>
+                        </div>
+                      </Stack>
+                    </div>
+                  </Stack>
+                </div>
+                <div style={{ cursor: "pointer", borderBottom: '#d6d6d6 solid 0.5px', marginBottom: '1rem' }} onClick={handleClickMatch}>
+                  <Stack direction="horizontal">
+                    <div>
+                      <p className='font-bold truncate-1-lines'>Table Tennis Ranking Match - APRIL 2024</p>
+
+                      <Stack direction="horizontal">
+                        <p className='truncate-1-lines font-14'>1 May 2024 - 31 May 2024</p>
+                        <div className='ms-auto' style={{ textAlign: 'right' }}>
+                          <Link style={{ color: "black" }}><p>Sign Up Now {">"}</p></Link>
+                        </div>
+                      </Stack>
+                    </div>
+                  </Stack>
+                </div>
+                <div style={{ cursor: "pointer" }} onClick={handleClickMatch}>
+                  <Stack direction="horizontal">
+                    <div>
+                      <p className='font-bold truncate-1-lines'>Table Tennis Ranking Match - APRIL 2024</p>
+
+                      <Stack direction="horizontal">
+                        <p className='truncate-1-lines font-14'>1 May 2024 - 31 May 2024</p>
+                        <div className='ms-auto' style={{ textAlign: 'right' }}>
+                          <Link style={{ color: "black" }}><p>Sign Up Now {">"}</p></Link>
+                        </div>
+                      </Stack>
+                    </div>
+                  </Stack>
+                </div>
+              </Card>
+            </Col>
+            <Col xs={8}>
+              <Card style={{ padding: '1.5rem', marginBottom: "0.5rem" }}>
+                <Stack direction='horizontal' gap={3}>
+                  <img src={testImg} alt="" width={40} height={40} />
+                  <div>
+                    <p className='font-16 font-bold' style={{ marginBottom: '0', marginLeft: '0.5rem' }}>Jesselball Sport Center, Simpang Ampat</p>
+                    <p className='font-14 font-italic' style={{ marginBottom: '0.3rem', marginLeft: '0.5rem', color: '#b7b7b7' }}>Now</p>
+                  </div>
+                </Stack>
+                <textarea className="input-textarea-feed font-14" placeholder="Enter something here..."> </textarea>
+                {/* <p className='font-14'>We are thrilled to announce the upcoming 2024 Spring Table Tennis Tournament! This exciting event will bring together players from various clubs to compete, showcase their skills, and share their passion for the game.</p> */}
+                <Button variant="primary" label={"Sign Up"} style={{width: '20%', margin: '0'}} className="ms-auto">
+              Post
+            </Button>
+              </Card>
+              <Card style={{ padding: '1.5rem', marginBottom: "0.5rem" }}>
+                <Stack direction='horizontal' gap={3}>
+                  <img src={testImg} alt="" width={40} height={40} />
+                  <div>
+                    <p className='font-16 font-bold' style={{ marginBottom: '0', marginLeft: '0.5rem' }}>Jesselball Sport Center, Simpang Ampat</p>
+                    <p className='font-14 font-italic' style={{ marginBottom: '0.3rem', marginLeft: '0.5rem', color: '#b7b7b7' }}>1 min ago..</p>
+                  </div>
+                </Stack>
+                <p className='font-14' style={{ lineHeight: '1.25rem', marginTop: '1rem', marginBottom: '0' }}>We are thrilled to announce the upcoming 2024 Spring Table Tennis Tournament! This exciting event will bring together players from various clubs to compete, showcase their skills, and share their passion for the game.</p>
+              </Card>
+              <Card style={{ padding: '1.5rem', marginBottom: "0.5rem" }}>
+                <Stack direction='horizontal' gap={3}>
+                  <img src={testImg} alt="" width={40} height={40} />
+                  <div>
+                    <p className='font-16 font-bold' style={{ marginBottom: '0', marginLeft: '0.5rem' }}>Jesselball Sport Center, Simpang Ampat</p>
+                    <p className='font-14 font-italic' style={{ marginBottom: '0.3rem', marginLeft: '0.5rem', color: '#b7b7b7' }}>1 min ago..</p>
+                  </div>
+                </Stack>
+                <p className='font-14' style={{ lineHeight: '1.25rem', marginTop: '1rem', marginBottom: '0' }}>We are thrilled to announce the upcoming 2024 Spring Table Tennis Tournament! This exciting event will bring together players from various clubs to compete, showcase their skills, and share their passion for the game.</p>
+              </Card>
+              <Card style={{ padding: '1.5rem', marginBottom: "0.5rem" }}>
+                <Stack direction='horizontal' gap={3}>
+                  <img src={testImg} alt="" width={40} height={40} />
+                  <div>
+                    <p className='font-16 font-bold' style={{ marginBottom: '0', marginLeft: '0.5rem' }}>Jesselball Sport Center, Simpang Ampat</p>
+                    <p className='font-14 font-italic' style={{ marginBottom: '0.3rem', marginLeft: '0.5rem', color: '#b7b7b7' }}>1 min ago..</p>
+                  </div>
+                </Stack>
+                <p className='font-14' style={{ lineHeight: '1.25rem', marginTop: '1rem', marginBottom: '0' }}>We are thrilled to announce the upcoming 2024 Spring Table Tennis Tournament! This exciting event will bring together players from various clubs to compete, showcase their skills, and share their passion for the game.</p>
+              </Card>
+            </Col>
+          </Row>
         </>
       )}
     </Container>
--- src/pages/Common/Ranking/index.jsx
+++ src/pages/Common/Ranking/index.jsx
@@ -10,22 +10,38 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Paper';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
+import { Link, useNavigate } from 'react-router-dom'
+
 import ListItemText from '@mui/material/ListItemText';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Stack } from 'react-bootstrap';
 import { FixedSizeList } from 'react-window';
+import Card from 'components/Card';
+import Avatar from 'assets/Avatar.png'
+
 function renderRow(props) {
   const { index, style } = props;
+  const navigate = useNavigate()
+
+  const handleClickPlayerProfile = () => {
+    navigate('/player/profile')
+  }
 
   return (
-    <ListItem style={style} key={index} component="div" disablePadding>
-      <Row style={{ width: '100%' }}>
-        <Col xs={2}>
-          1
-        </Col>
-        <Col>
-          Hia Wei Qi
-        </Col>
-      </Row>      {/* <ListItemButton>
+    <ListItem style={style} key={index}>
+      <Card style={{ padding: '1rem', width: '100%', alignSelf: 'center' }}>
+        <Row style={{ width: '100%', alignItems: 'center' }}>
+          <Col xs={2}>
+            {index + 1}
+          </Col>
+          <Col>
+            <Stack direction='horizontal' gap={0} style={{ alignSelf: 'center', padding: '1rem', cursor: 'pointer' }} onClick={handleClickPlayerProfile}>
+              <img src={Avatar} alt="" width={40} height={40} />
+              <p className='font-20 font-bold' style={{ marginBottom: '0.3rem', marginLeft: '0.5rem' }}> Opponent 1</p>
+
+            </Stack>          
+            </Col>
+        </Row>
+      </Card>      {/* <ListItemButton>
         <ListItemText primary={`Item ${index + 1}`} />
       </ListItemButton> */}
     </ListItem>
@@ -35,19 +51,14 @@ function renderRow(props) {
 export default function Ranking() {
   return (
     <Container style={{ marginTop: '3rem' }}>
-      Ranking
-      <Box
-        sx={{ width: '100%', height: 540, bgcolor: 'background.paper' }}
+      <FixedSizeList
+        height={540}
+        itemSize={128}
+        itemCount={200}
+        overscanCount={5}
       >
-        <FixedSizeList
-          height={540}
-          itemSize={100}
-          itemCount={200}
-          overscanCount={5}
-        >
-          {renderRow}
-        </FixedSizeList>
-      </Box>
+        {renderRow}
+      </FixedSizeList>
     </Container>
   )
 }
